c0e7366b82bf18cdbda067470c552f6dec1c2d81
David Iturriaga Sotelo <e-diturriaga@neoris.com>
2021-03-06 14:22:11 -0800

    Image Background

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,  {useState} from 'react';
-import { Image, View, StyleSheet, Switch } from 'react-native';
+import { Image, View, StyleSheet, Switch, ImageBackground } from 'react-native';
 
 export default function App() {
 
@@ -15,11 +15,11 @@ export default function App() {
   
   return (
     
-    <View style={styles.container}>
+    <ImageBackground source={ icon } style={styles.container}>
       <Image 
         source={{ uri: 'https://image.freepik.com/foto-gratis/retrato-gato-sobre-fondo-blanco_13339-114852.jpg' }} 
         style={styles.image}
-        />
+      />
       
       <Switch
         trackColor={{ false: "#767577", true: "#81b0ff" }}
@@ -29,7 +29,7 @@ export default function App() {
         value={isEnabled}
       />
     
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -39,6 +39,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%', 
+    height: '100%'
   },
   image:{
     width: 200, 
