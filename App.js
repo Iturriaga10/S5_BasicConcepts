582b3c7a794eb6b09df419bceeb4ed1f943ab1ea
David Iturriaga Sotelo <e-diturriaga@neoris.com>
2021-03-06 13:27:46 -0800

    Reference Types example

--- App.js
+++ App.js
@@ -4,22 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  var testVar = 'Tec'; // String
-  //testVar = "Tec"; // String
-
-  //var testVar = null; // Null
-  //testVar = 'TEC'; // String
-  //testVar = 5; // Number
-  
-  //var testVar = undefined; // Undefinded
-  //testVar = 'TEC'; // String
-  //testVar = 5; // Number
-
-  //var testVar = false; // Boolean
-
-  //var testVar = Symbol("Tec"); // Symbol
-  //var testVar1 = Symbol("Tec"); // Symbol
-  //console.log(testVar === testVar1)
+  var testVar = ['Tec', 'de','Monterrey'];
 
   return (
     <View style={styles.container}>
