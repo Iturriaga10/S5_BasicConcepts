7e4a0c5511de8f2ccef5f30821cedd09c9d84216
David Iturriaga Sotelo <e-diturriaga@neoris.com>
2021-03-06 13:22:34 -0800

    Primitive Types

--- App.js
+++ App.js
@@ -3,9 +3,28 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  var testVar = 'Tec'; // String
+  //testVar = "Tec"; // String
+
+  //var testVar = null; // Null
+  //testVar = 'TEC'; // String
+  //testVar = 5; // Number
+  
+  //var testVar = undefined; // Undefinded
+  //testVar = 'TEC'; // String
+  //testVar = 5; // Number
+
+  //var testVar = false; // Boolean
+
+  //var testVar = Symbol("Tec"); // Symbol
+  //var testVar1 = Symbol("Tec"); // Symbol
+  //console.log(testVar === testVar1)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your {testVar} app!</Text>
+      <Text>Tipo de Variable: {typeof(testVar)}</Text>
       <StatusBar style="auto" />
     </View>
   );
